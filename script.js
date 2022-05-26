4a76fa838615fb7752fa65d6825aa9951919ef2a
DHARMENDRA KUMAR GOND <dgond0001@gmail.com>
2022-05-26 14:47:49 +0530

    Update soming

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Developer", "Blogger", "Designer", "Gamer"],
+        strings: ["Web Developer", "Developer", "Blogger", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Developer", "Blogger", "Designer", "Gamer"],
+        strings: ["Web Developer", "Developer", "Blogger", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
